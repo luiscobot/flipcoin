bd3d715bd6de6f3b076d46474a32be290b058d7b
Luis Romero <diceluisco@gmail.com>
2020-11-03 01:55:25 +0000

    eliminar mensaje en consola.

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ var token = document.querySelector(".token");
 var ganador = document.querySelector(".ganador");
 var guía = document.querySelector(".guía");
 var info = document.querySelector(".info-plus");
-var elementosParaAnimar = [moneda, moneda.parentNode, guía, info, token.parentNode];
+var elementosParaAnimar = [
+  moneda,
+  moneda.parentNode,
+  guía,
+  info,
+  token.parentNode,
+];
 var tarjeta = document.querySelector(".info");
 var estáGirando = false;
 var ladoActual = "heads";
@@ -11,85 +17,67 @@ var ladoPróximo;
 var animación;
 
 function controladorOnLoad() {
-	if (document.visibilityState === "visible") animarElementos();
-	document.addEventListener("visibilitychange", controladorVisibilityChange);
-	window.removeEventListener("load", controladorOnLoad);
+  if (document.visibilityState === "visible") animarElementos();
+  document.addEventListener("visibilitychange", controladorVisibilityChange);
+  window.removeEventListener("load", controladorOnLoad);
 }
 
 function controladorVisibilityChange() {
-	if (document.visibilityState === "visible") animarElementos();
-	document.removeEventListener("visibilitychange", controladorVisibilityChange);
+  if (document.visibilityState === "visible") animarElementos();
+  document.removeEventListener("visibilitychange", controladorVisibilityChange);
 }
 
 function animarElementos() {
-	elementosParaAnimar.forEach(function(elemento) {
-		elemento.classList.add("inicio");
-	});
-	
-	moneda.addEventListener("animationend", function() {
-		this.classList.remove("inicio");
-	})
+  elementosParaAnimar.forEach(function (elemento) {
+    elemento.classList.add("inicio");
+  });
+
+  moneda.addEventListener("animationend", function () {
+    this.classList.remove("inicio");
+  });
 }
 
 function lanzarMoneda() {
-	if (estáGirando) return false;
-	estáGirando = true;
+  if (estáGirando) return false;
+  estáGirando = true;
 
-	ganador.classList.remove("resultado");
+  ganador.classList.remove("resultado");
 
-	ladoPróximo = Math.round(Math.random()) ? "heads" : "tails";
-	animación = ladoActual + "-" + ladoPróximo;
+  ladoPróximo = Math.round(Math.random()) ? "heads" : "tails";
+  animación = ladoActual + "-" + ladoPróximo;
 
-	moneda.style.setProperty("animation-name", animación);
-	moneda.classList = `moneda ${animación}`;
+  moneda.style.setProperty("animation-name", animación);
+  moneda.classList = `moneda ${animación}`;
 
-	moneda.addEventListener("animationend", function() {
-		moneda.style.removeProperty("animation-name");
-		estáGirando = false;
+  moneda.addEventListener("animationend", function () {
+    moneda.style.removeProperty("animation-name");
+    estáGirando = false;
 
-		ganador.textContent = ladoPróximo;
-		ganador.classList.add("resultado");
+    ganador.textContent = ladoPróximo;
+    ganador.classList.add("resultado");
 
-		ladoActual = ladoPróximo;
-	});
+    ladoActual = ladoPróximo;
+  });
 }
 
 function mostrarTarjeta() {
-	tarjeta.classList.add("mostrar-info");
+  tarjeta.classList.add("mostrar-info");
 }
 
 function ocultarTarjeta() {
-	tarjeta.classList.remove("mostrar-info");
+  tarjeta.classList.remove("mostrar-info");
 }
 
 function controladorClicEnTarjeta(evento) {
-	if (evento.target !== tarjeta) return false;
-	ocultarTarjeta();
+  if (evento.target !== tarjeta) return false;
+  ocultarTarjeta();
 }
 
 function controladorKeyUp(evento) {
-	if (!tarjeta.classList.contains("mostrar-info")) return false;
-	if (evento.key !== "Escape") return false;
-	ocultarTarjeta();
+  if (!tarjeta.classList.contains("mostrar-info")) return false;
+  if (evento.key !== "Escape") return false;
+  ocultarTarjeta();
 }
-
-
-var mensaje =
-`
-8888888                                      888                          d8b          888
-  888                                        888                          Y8P          888
-  888                                        888                                       888
-  888        888  888  888  8888b.  88888b.  888888       8888b.         8888  .d88b.  88888b.       Hi, my name is Luis Romero,
-  888        888  888  888     "88b 888 "88b 888             "88b        "888 d88""88b 888 "88b      I am a Junior Front-End Developer living in Colombia,
-  888        888  888  888 .d888888 888  888 888         .d888888         888 888  888 888  888      and I would like to get my first front-end developer job
-  888        Y88b 888 d88P 888  888 888  888 Y88b.       888  888         888 Y88..88P 888 d88P      here in Bogotá, or Remote,
-8888888       "Y8888888P"  "Y888888 888  888  "Y888      "Y888888         888  "Y88P"  88888P"       my email is: hola@luisromero.co
-                                                                          888
-                                                                         d88P
-                                                                       888P"
-`
-
-console.log(mensaje);
 
 window.addEventListener("load", controladorOnLoad);
 moneda.addEventListener("click", lanzarMoneda);
@@ -98,10 +86,10 @@ info.addEventListener("click", mostrarTarjeta);
 tarjeta.addEventListener("click", controladorClicEnTarjeta);
 window.addEventListener("keyup", controladorKeyUp);
 
-token.addEventListener("click", function() {
-	token.classList.add("giro");
+token.addEventListener("click", function () {
+  token.classList.add("giro");
 
-	token.addEventListener("animationend", function() {
-		token.classList.remove("giro");
-	});
+  token.addEventListener("animationend", function () {
+    token.classList.remove("giro");
+  });
 });
